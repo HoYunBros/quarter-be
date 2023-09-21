615e33a3d93ac0a25c7fb642e8b6824d04162dd3
ggam-nyang <v44ads@naver.com>
2023-09-22 03:02:45 +0900

    jenkins test

---
+++ src/main/kotlin/io/ggamnyang/quarter/main/controller/JenkinsTestController.kt
@@ -0,0 +1,13 @@
+package io.ggamnyang.quarter.main.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class JenkinsTestController {
+
+    @GetMapping("/test")
+    fun testJenkins(): String {
+        return "Hello, Jenkins CI CD WORLD!!!"
+    }
+}
